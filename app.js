0aa30e5f0e3ee3661d0f1bad27f37ee873b47c75
solowei88 <solovev.aleksey1988@yandex.ru>
2025-02-25 01:30:43 +0300

    12 Арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-let a = 1;
-console.log(a);
-a = 'test';
-console.log(a);
+//Базовые арифметические операторы
+const a = 1;
+const b = 2;
+const c = 3;
+const d = 4;
+const e = 5;
+const f = 6;
+const g = 7;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b ** c); // 2 в третьей степени. 2х2х2
+
+//Строки
+const city = 'Москва'
+const street = 'Новгородская'
+const adres = city +', '+ street +', 5'
+console.log(adres);
+
+
