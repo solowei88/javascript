379717536d53fa54f2acceb6fd62b02d884d42fc
solowei88 <solovev.aleksey1988@yandex.ru>
2025-03-10 10:25:24 +0300

    If else

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 /*
-False из других типов
+If else
 */
 
-console.log(Boolean(0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));
+const money = 100;
+//const maybeBuy = money > 50; 
+
+if (money > 50) {
+    console.log('Может купить наш продукт');
+} else if (money > 5) {
+    console.log('Курлен мини продукт');
+} else {
+    console.log('Не хватает денег');
+}
+console.log('Итог');
 
 
