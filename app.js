d0612beb3eaaa03e5add0c76e911c20da2cb0862
solowei88 <solovev.aleksey1988@yandex.ru>
2025-03-11 18:15:40 +0300

    Логические операторы
     и, или, не

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 /*
-Упражнение - Проверка робота
+Логические операторы
 */
 
-let res = prompt ('Сколько будет 7 + или - 15');
+const isAdmin = true;
+const canWrite = false;
 
-
-let message; 
-/*
-if (res ==='Я не робот') {
-    message = 'Успех';
-} else if (Number(res) === (7 + 15)){
-    message = 'Успех';
-} else if (Number(res) === (7 - 15)){
-    message = 'Успех';
-} else {
-    message = 'Вы робот';
-}
-*/
-switch (true) {
-    case res === "Я не робот": // Проверка на строку "Я не робот"
-        message = 'Успех';
-    case Number(res) === 7 + 15: // Проверка на 22
-    case Number(res) === 7 - 15: // Проверка на -8
-        message = 'Успех';
-        break;
-    default:
-        message = 'Вы робот';
-        break;
-}
-
-
-console.log (res);
-console.log (` ${message}`);
+console.log (`Системный файл ${isAdmin && canWrite}`); // и
+console.log (`Обычный файл ${isAdmin || canWrite}`); // или
+console.log (`Инвертируем права админа ${!isAdmin}`); // не
 
 
