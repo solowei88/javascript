501627f47c397b0c2c6a923e0f71a317b8221e67
solowei88 <solovev.aleksey1988@yandex.ru>
2025-03-15 14:09:40 +0300

    Введение в функции

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 /*
- Упражнение - Проверка прав
+ Введение в функции
 */
-/*
+
 function logName (name, surname) {
     console.log (`My name is ${name} ${surname}`);
 }
 
 logName ('Alexey', 'Ernst');
 logName (1, 3);
-*/
+
 function countDepositSum (depositUSD, month, rate) {
     let sum = depositUSD * (1 + rate / 12) ** month;
     
     return sum;
 }
+
 /* вариант 2 
 function countDepositSum (depositUSD, month, rate) {
     return depositUSD * (1 + rate / 12) ** month;
