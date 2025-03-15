a02789888df6e1f4b876a4ca52e9daf2b47a250c
solowei88 <solovev.aleksey1988@yandex.ru>
2025-03-15 18:27:11 +0300

    Анонимные функции

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 /*
- Введение в функции
+Анонимные функции
 */
 
-function logName (name, surname) {
-    console.log (`My name is ${name} ${surname}`);
+function powerOfTwo (num) {
+    return num * num;
 }
 
-logName ('Alexey', 'Ernst');
-logName (1, 3);
 
-function countDepositSum (depositUSD, month, rate) {
-    let sum = depositUSD * (1 + rate / 12) ** month;
+console.log (powerOfTwo (4));
+
+const poft = function (num){
+    return num * num;
+}
     
-    return sum;
-}
-
-/* вариант 2 
-function countDepositSum (depositUSD, month, rate) {
-    return depositUSD * (1 + rate / 12) ** month;
-
-}
-*/
-
-const output1 = countDepositSum (1000,24,0.12);
-
-console.log (`My name is ${output1}`);
-
-const output2 = countDepositSum (1000,36,0.12);
-
-console.log (`My name is ${output2}`);
-
-
-    
-
+console.log (poft (4));
 
 
 
