c308cecb19d42a769d4f486f4ebc5b7cbb9feb9f
solowei88 <solovev.aleksey1988@yandex.ru>
2025-04-30 22:04:53 +0300

    Упражнение - Кредит на MacBook

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 /*
-Функции в функциях
+Упражнение - Кредит на MacBook
 */
-const KG_IN_USD = 7;
-const KM_IN_USD = 5;
+/*
+	Пользователь:
+	- Возраст
+	- Наличие работы
+	- Деньги
+	Нужно проверить может ли он купить новый MacBook за 2000$?
+	Он может брать не только свои деньги, но и взять кредит.
+	Ему дадут 500$, только если ему больше 24-х лет и он
+	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
+	ином случае.
+	Напишите функцию, которая принимает данные пользователя
+	и товара и возвращает true или false;
+*/
+const AGE = 25;
+const WORK = false;
+const MONEY = 1900;
+const CREDIT = creditUSD(AGE,WORK)
 
-function calculateW (present){
-    return present * KG_IN_USD;
+function creditUSD (age, work){
+    if (age > 24 && work === true){
+        return 500;
+    } else if (AGE > 24){
+        return 100;
+    } else {
+        return 0;
+    }
+    
 }
 
-function calculateKm (distance){
-    return distance * KM_IN_USD;
+
+function calculation (money) {
+    if (money >= 2000) {
+        return true;
+    } else if (money + CREDIT >= 2000){
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function getExchangePrise(present1,present2,distance) {
-    const price1 = calculateW (present1);
-    const price2 = calculateW (present2);
-    const distancePrice = calculateKm (distance);
-    return price1 + price2 + distancePrice;
-}
-
-
-
-console.log (getExchangePrise(1,2,10));
+console.log (creditUSD(AGE,WORK));
+console.log (calculation(MONEY));
 
 
 
