9a7cf48a8c8b54b16de34f21e9e0b1b9b07117d6
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-03 06:20:34 +0300

    Из строки в массив и обратно

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
 /*
-Slice, splice, concat, reverse
+Из строки в массив и обратно
 */
 
 const roles = ['user', 'admin', 'manager', 'superuser'];
-const res = roles.slice(2);     //берёт элементы, но не вырезает из массива
-console.log (roles);
-console.log (res);
+const url = 'auth/user/login';
+const res = url.split('/') ; //преобразует строку в массив
+console.log(res);
 
-const res2 = roles.slice(0, 3);  //берёт элементы, но не вырезает из массива
-console.log (roles);
-console.log (res2);
-
-const res3 = roles.slice(-1);    //берёт последний элемент, но не вырезает из массива
-console.log (roles);
-console.log (res3);
-
-const res4 = roles.slice(1,-1);  //берёт элементы, но не вырезает из массива
-console.log (roles);
-console.log (res4);
-
-//const res5 = roles.splice(2);     //вырезает элементы из массива
-//console.log (roles);
-//console.log (res5);
-//console.log (roles);
-
-// const res6 = roles.splice(2, 2); //вырезает элементы из массива
-// console.log (roles);
-// console.log (res6);
-// console.log (roles);
-
-console.log (roles);
-const res8 = roles.reverse(); // Меняет элементы массива задом на перёд
-console.log (roles);
-
-const NEW_ROLES = ['sysadmin','developer']
-const res9 = roles.concat(NEW_ROLES)  //Соединяет массивы
-
-console.log (res9);
+console.log(roles.join('-')) ;//преобразует массив в строку
