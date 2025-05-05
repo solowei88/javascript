47c222147b2fa0bd2ee1bfe14c6b8f7a170fa6d3
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-06 00:23:22 +0300

    Упражнение - Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 /*
-Break и continue
+Упражнение - Цикл в обратном порядке
 */
-const TASKS = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4'];
-
-for (i = 0 ; i < TASKS.length; i++){
-    if (TASKS[i] === 'Задача 1'){
-        continue;                            // пропускает 1 итерацию
-    } else if (TASKS[i] === 'Задача 4'){
-        break;                               // останавливает выполнение цикла
-    }
-    console.log(TASKS[i]);
+const ARR = ['!', 'js', 'люблю', 'Я'];
+const RESULT_ARRAY = [];
+for (i = ARR.length - 1 ; i >= 0; i--){    
+    RESULT_ARRAY.push(ARR[i])    
 }
+const ARR3 = RESULT_ARRAY.join(" ");
+console.log(ARR3);
+
+console.log(ARR.reverse().join(" ")); //то же самое через методы reverse().join(" ")
 
 
 
