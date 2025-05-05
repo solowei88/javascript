ffa1fa358eaf2eb78516b3eea4964fbc60525fec
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-05 22:52:17 +0300

    Break и continue

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 /*
-Цикл for
+Break и continue
 */
+const TASKS = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4'];
 
-for (i = 1 ; i < 10; i++){
-    console.log(`Баланс: ${i}$`);
+for (i = 0 ; i < TASKS.length; i++){
+    if (TASKS[i] === 'Задача 1'){
+        continue;                            // пропускает 1 итерацию
+    } else if (TASKS[i] === 'Задача 4'){
+        break;                               // останавливает выполнение цикла
+    }
+    console.log(TASKS[i]);
 }
 
 
