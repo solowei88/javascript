66b96dbec1cd456fde0c1914208579dfe31956d1
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-07 02:39:13 +0300

    Цикл while

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 /*
-Цикл в цикле
+Цикл while
 */
 
-// for (i = 1 ; i < 5; i++){    
-//     console.log(`Цикл 1 - ${i}`);  
-//     for (j = 0 ; j < 5; j++){
-//         console.log(`Цикл 2 - ${j}`);
+
+const ARR = [1,2,4,5,11,7];
+
+// for (i = 0 ; i < ARR.length ; i++){     
+//     if (ARR[i] > 5){
+//         break;
 //     }
+//     console.log(ARR[i]);
        
 // }
-const tasks = [[1,'Задача 1'], [2,'Задача 2']];
 
-for (i = 0 ; i < tasks.length ; i++){     
-    for (j = 0 ; j < tasks[i].length; j++) {
-        console.log(tasks[i][j]);
-    }
-       
+let i = 0
+while (ARR[i] <= 5 && i < ARR.length) {
+      console.log(ARR[i]);
+      i++;
 }
 
+
+let j = 0
+do {
+    console.log(j);
+    j++;
+} while (j < 0);  // do while - одна итерация выполнится в любом случае
