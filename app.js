1a91646cbcbe5375d4e5d867178e3c3161fef2d7
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-07 16:26:00 +0300

    Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 /*
-Циклы for of и for in
+Упражнение - Расчёт итогового баланса
 */
 
 
-const ARR = [1,2,4,5,11,7,'Циклы'];
+const operations = [1000,-700,300,-500,10000];
+let sum1 = 0;
+let sum2 = 0;
+let sum3 = 0;
 
-for (let element of ARR) {
-    console.log(element);  //вывадит элементы массива
+function summaBolshe0(summa){
+    if (summa < 0){
+        return false;
+    }
+    return true;
+        
+}
+console.log(summaBolshe0(sum3));
+function summa(arr){
+    for (let element of arr) {
+        sum1 = sum1 + element;
+        if (!summaBolshe0()){
+            break;
+        }
+    }
+    console.log(`Сумма: `+ sum1);
 }
 
+summa(operations);
 
-for (let index in ARR) {
-    console.log(index);   //вывадит индексы элементов массива
+function srednyDohod(arr){
+    let i = 0;
+    for (let element of arr) {
+        if (element >= 0 ){
+            sum2 = sum2 + element; 
+            i++;   
+        }
+        
+    }
+    console.log(`Средний доход: `+ sum2 / i);
 }
+    
+srednyDohod(operations);  
+
+function srednyRashod(arr){
+    let g = 0;
+    for (let element of arr) {
+        if (element <= 0 ){
+            sum3 = sum3 - element; 
+            g++;   
+        }
+        
+    }
+    console.log(`Средний расход: `+ sum3 / g);
+}
+    
+srednyRashod(operations);
+
