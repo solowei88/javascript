c42ccf62a828dcb1707cf1bd6ab9fad845011c2a
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-07 05:58:31 +0300

    Циклы for of и for in

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 /*
-Цикл while
+Циклы for of и for in
 */
 
 
-const ARR = [1,2,4,5,11,7];
+const ARR = [1,2,4,5,11,7,'Циклы'];
 
-// for (i = 0 ; i < ARR.length ; i++){     
-//     if (ARR[i] > 5){
-//         break;
-//     }
-//     console.log(ARR[i]);
-       
-// }
-
-let i = 0
-while (ARR[i] <= 5 && i < ARR.length) {
-      console.log(ARR[i]);
-      i++;
+for (let element of ARR) {
+    console.log(element);  //вывадит элементы массива
 }
 
 
-let j = 0
-do {
-    console.log(j);
-    j++;
-} while (j < 0);  // do while - одна итерация выполнится в любом случае
+for (let index in ARR) {
+    console.log(index);   //вывадит индексы элементов массива
+}
