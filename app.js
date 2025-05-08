35efcf14f87a4a761a4e0babd963739fe901d148
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-08 08:29:20 +0300

    Функции высшего порядка. Callback.

--- app.js
+++ app.js
@@ -1,58 +1,37 @@
 /*
-Упражнение - Расчёт итогового баланса
+Функции высшего порядка. Callback.
 */
 
 
-const operations = [1000,-700,300,-500,10000];
-let sum1 = 0;
-let sum2 = 0;
-let sum3 = 0;
 
-function summaBolshe0(summa){
-    if (summa < 0){
-        return false;
-    }
-    return true;
-        
-}
-console.log(summaBolshe0(sum3));
-function summa(arr){
-    for (let element of arr) {
-        sum1 = sum1 + element;
-        if (!summaBolshe0()){
-            break;
-        }
-    }
-    console.log(`Сумма: `+ sum1);
+
+function positive(a, b){
+    const res = a + b ;
+    return res;       
 }
 
-summa(operations);
-
-function srednyDohod(arr){
-    let i = 0;
-    for (let element of arr) {
-        if (element >= 0 ){
-            sum2 = sum2 + element; 
-            i++;   
-        }
-        
-    }
-    console.log(`Средний доход: `+ sum2 / i);
+function negative(a, b){
+    const res = a - b ;
+    return res;       
 }
-    
-srednyDohod(operations);  
 
-function srednyRashod(arr){
-    let g = 0;
-    for (let element of arr) {
-        if (element <= 0 ){
-            sum3 = sum3 - element; 
-            g++;   
-        }
-        
-    }
-    console.log(`Средний расход: `+ sum3 / g);
+function umnojenie(a, b){
+    const res = a * b ;
+    return res;       
 }
-    
-srednyRashod(operations);
 
+function delenie(a, b){
+    const res = a / b ;
+    return res;       
+}
+
+function callback(a, b, fn){ //функция высшего порядка (принимает другую функцию)
+    console.log(fn.name);
+    const res = fn(a,b);
+    return res;       
+}
+
+console.log(callback(2, 3, delenie));
+console.log(callback(2, 3, umnojenie));
+console.log(callback(2, 3, negative));
+console.log(callback(2, 3, positive));
