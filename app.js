394b78c864bf2fde61a7a117242ce0ff468964c7
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-11 13:01:57 +0300

    forEach

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 /*
-Упражнение - Стрелочные функции
+forEach
 */
 
+const score = [1, 5, 7, 9];
 
+// for (const [i, el] of score.entries()) {
+//     console.log(`Раунд: ${i}, ${el}`);
+// }
 
-
-function power(pow){      
-    
-    return function(num) {
-        return num**pow;
-    }    
-}
-
-const powerOfTwo = power(2); // кладём функцию в константу
-console.log(powerOfTwo(3));
-console.log(powerOfTwo(4));
-
-const powerOfTree = power(3);
-console.log(powerOfTree(3));
-console.log(powerOfTree(5));
-
-console.log(power(5)(4));  // передаём аргументы в две функции
-
-
-const power2 = (pow) => (num) => num**pow; // стрелочная функция такая запись тоже правильная const power2 = pow => num => num**pow ;
-console.log(power2(2)(3));
+score.forEach((el, i) => {
+    console.log(`Раунд: ${i} - ${el}`);
+  });
