de5adf112e086dfb3e85ff7ecc6c2b09d09d3b02
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-14 22:26:31 +0300

    find и findIndex

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 /*
-reduce Упражнение - среднее значение
+find и findIndex
 */
 
-const operations = [100, 200, -120, 100, -200, 350];
+const operations = [100, 200, -120, 300, -200, 350];
+// for (el of operations){
+//     if(el > 200){
+//         console.log(el);
+//         break;
+//     }
+// }
 
+const elGT200 = operations.find(el => el > 100);
+console.log(elGT200);
 
+const elGT200Index = operations.findIndex(el => el > 100);
+console.log(elGT200Index);
 
-const avg = operations.reduce((acc, value, i) =>{
-    acc += value;
-    return acc / (i + 1);    
-},0)
-console.log(avg);
 
 
 
