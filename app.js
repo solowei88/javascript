4f998f326b2256796b47c22bb6127fe41a1336fd
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-14 22:09:50 +0300

    reduce Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 /*
-reduce
+reduce Упражнение - среднее значение
 */
 
 const operations = [100, 200, -120, 100, -200, 350];
 
-let summ = 0;
-for (money of operations){
-    summ += money;
-    console.log(summ)
-}
 
-const finalBalanse = operations.reduce((acc, value, i) =>{
-    return acc += value;
-},0)
-console.log(finalBalanse);
 
-const minElement = operations.reduce((acc, value, i) =>{
-    if (acc < value){
-        return acc ;
-    } else {
-       return value ; 
-    }
-    
+const avg = operations.reduce((acc, value, i) =>{
+    acc += value;
+    return acc / (i + 1);    
 },0)
-console.log(minElement);
+console.log(avg);
+
+
+
