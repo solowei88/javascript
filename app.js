a8b98a723f81d5262c2a44dec1c0b968f44bdbd3
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-16 11:07:53 +0300

    Быстрое создание массивов

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 /*
-sort
+Быстрое создание массивов
 */
 
-const users = ['Вася', 'Петя', 'Маша', 'Глаша'];
-
-console.log(users);
-
-users.sort(); // сортировка по алфавиту
-
-console.log(users);
-
-const operations = [100, -300, -100, 50, 480];
-
-console.log(operations);
-
-operations.sort((a, b) => {  // сортировка по убыванию
-    if(a < b){
-        return 1;
-    }
-    if(a > b) {
-        return -1;
-    }
-});
-
-console.log(operations);
-
-operations.sort((a, b) => a - b); // сортировка по возростанию
-
-console.log(operations);
+const arr = [1, 2, 3, 4, 5];
+console.log(new Array(1, 2 ,3 ,4 ,5));
 
 
+const arr2 = new Array(5);
+console.log(arr2);
+arr2.fill(1,0,3);
+arr2.fill(2,3,5);
+console.log(arr2);
 
 
+const arr3 = Array.from( {length:5}, (cur, i) => i + 1 );
 
+console.log(arr3);
 
