7d8916a6dc7cc5c4ac27d1c549e9d5ae32936cc8
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-17 21:48:04 +0300

    Базовые методы

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 /*
-Быстрое создание массивов
+Базовые методы
 */
 
-const arr = [1, 2, 3, 4, 5];
-console.log(new Array(1, 2 ,3 ,4 ,5));
+const userName = 'Вася Пупкина'
+console.log(userName[0]);
+console.log(userName[1]);
+console.log(userName[2]);
+console.log(userName.charAt(2));
+console.log(userName.length);
+console.log(userName.indexOf('уп'));
+console.log(userName.lastIndexOf('а'));
+console.log(userName.includes('уп'));
+console.log(userName.slice(6));
+console.log(userName.slice(5, 8));
 
-
-const arr2 = new Array(5);
-console.log(arr2);
-arr2.fill(1,0,3);
-arr2.fill(2,3,5);
-console.log(arr2);
-
-
-const arr3 = Array.from( {length:5}, (cur, i) => i + 1 );
-
-console.log(arr3);
 
