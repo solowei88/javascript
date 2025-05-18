a566bd9cc7a0cf7eb6eac9f987235e362783d35b
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-18 13:01:39 +0300

    Преобразование строки

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 /*
- Упражнение - выделение имени
+Преобразование строки 
 */
 
 const userName = 'Вася aka Terminator Пупкин'
 
 const userName1 = userName.slice(0, userName.indexOf(' '));
+
 console.log(userName1);
 
 const userName2 = userName.slice(userName.lastIndexOf(' ') + 1);
+
 console.log(userName2);
 
 const fullUserName = `${userName1} ${userName2}`;
-console.log(fullUserName);
+
+console.log(fullUserName); 
 
 
+   
