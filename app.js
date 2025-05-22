3429b99a8eeb3e4eb76501e7536efc10b0616848
solowei88 <solovev.aleksey1988@yandex.ru>
2025-05-22 20:32:05 +0300

    Упражнение - сортировка пользователей

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 /*
-Обращение к элементам
+Упражнение - сортировка пользователей
 */
 
 
-const userArrey = ['Вася','Пупкин', 24];
+const users = [
+    {name: 'Вася', age: 54},
+    {name: 'Катя', age: 34},
+    {name: 'Аня', age: 44},
+    {name: 'Петя', age: 24},    
+];
 
-const user = {
-    name: 'Вася', 
-    surname:'Пупкин',
-    age: 24,
-    skills: [
-        'Программирование', 'готовка'
-    ],
-    eduBasic: 'школа',
-    eduPro: 'Институт'
-};
-console.log(user);
-console.log(user.name);
-console.log(user.skills[0]);
-console.log(user.age);
+console.log(users.sort((a , b) => a.age - b.age));
 
-console.log(user['skills']);
-console.log(user['age']);
-
-const level = 'Pro';
-console.log(user['edu' + level]);
-
-// const res = prompt('Ведите свойство: ');
-// console.log(res);
-// console.log(user[res]);
-
-user['city'] = 'Москва';
-console.log(user.city);
-
-user.age = 30;
-console.log(user.age);
-
-user.style = 'хороший';
-console.log(user.style);
