f51f28670e6f95e71f34ca9c3a00bc1d3aad39e1
solowei88 <solovev.aleksey1988@yandex.ru>
2025-07-03 11:59:04 +0300

    Упражнение - склад

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 /*
-Optional chaining
+Упражнение - склад
 */
 
 
-const cities = {
-  MSK: {
-    temp: {
-      celsius: 20
-    }
+const werehouse = {
+  goods: [],
+  findGoodById: function (id){
+    return this.goods.find(g => g.id === id);
   },
-  SPB: {
-    
-  }
+  addGood: function (good){
+    this.goods.push(good);
+  },
+  getWeightKg: function () {
+    return this.goods.reduce((acc, el) => 
+      acc += el.weight?.kg ? el.weight.kg : 0, 0)
+  },    
 };
-console.log(cities.SPB?.temp?.celsius);
+ 
+const car = {
+  id: 1,
+  weight: {
+    kg:1000
+  },
+  brand: 'Ford'
+}
+
+const chair = {
+  id: 2,
+  weight: {
+    kg:2
+  },
+}
+
+const paper = {
+  id: 3,
+  color: 'red' 
+}
+
+werehouse.addGood(car);
+werehouse.addGood(chair);
+werehouse.addGood(paper);
+
+
+console.log(werehouse.goods);
+console.log(werehouse.findGoodById(1));
+console.log(werehouse.getWeightKg());
 
